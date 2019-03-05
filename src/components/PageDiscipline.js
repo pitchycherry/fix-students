4f51eb241e2fb1212abe95962715d245be4b9105
AnatoliyCH <googlsocial97@gmail.com>
2019-03-06 00:53:21 +0700

    PageStudent

---
+++ src/components/PageDiscipline.js
@@ -0,0 +1,12 @@
+import React, {Component, Fragment} from "react";
+import {Header} from "./Header";
+
+export class PageDiscipline extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Header/>
+            </Fragment>
+        )
+    }
+}
