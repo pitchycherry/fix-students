e4c8981a2b23e24d2d48b6a5bb4d9a9e143dbbf9
AnatoliyCH <googlsocial97@gmail.com>
2019-03-12 18:35:48 +0700

    Discipline 88%

--- src/components/ListDiscipline.js
+++ src/components/ListDiscipline.js
@@ -1,130 +1,58 @@
 import React, {Component, Fragment} from "react";
 import 'font-awesome/css/font-awesome.min.css';
 
-const Disciplines = [
-    {
-        id: 1, // добавили id
-        title: "Пбработка сигналов"
-    },
-    {
-        id: 2, // добавили id
-        title: "БЖД"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 2, // добавили id
-        title: "БЖД"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    },
-    {
-        id: 3, // добавили id
-        title: "Право"
-    }
-
-];
 export class ListDiscipline extends Component {
+    state = {
+        arr: null,
+        isLoading: false,
+    };
+    loadList = () =>{
+        fetch('http://nstu-tracker.thematrix.su/discipline',{
+            method: "GET",
+            headers:{"api-token": "JqHNSZ7YjoAFVqg43hPfDnvA"}
+        }).then(function (response) {
+            return response.json()
+        }).then(data =>{
+            this.setState({isLoading:false, arr: data.data});
+        }).catch(function(error) {
+            console.log('POST failed', error.message)
+        })
+    };
+    componentDidMount() {
+        this.setState({isLoading:true});
+        this.loadList();
+    }
     render() {
-        const DisciplineArr = Disciplines.map(function (item) {
-            return(
-            <li className="list-group-item" key={item.id}>
+        const {arr, isLoading} = this.state;
+        let DisciplineList;
+        if (!!arr) {
+            DisciplineList = arr.map(function (item) {
+                return (
+                    <li className="list-group-item" key={item.id}>
+                        <div className="row">
+                            <div className="col text-left name-group">{item.name}</div>
+                            <div className="col text-right">
+                                <button type="button" className="btn" data-toggle="modal"
+                                        data-target="#editDisciplineModal">
+                                    <i className="fas fa-pencil-alt"></i>
+                                </button>
+                                <button type="button" className="btn" data-toggle="modal"
+                                        data-target="#deleteDisciplineModal">
+                                    <i className="fas fa-trash-alt"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </li>
+                )
+            })
+        }else{
+            DisciplineList =
+            <li className="list-group-item" key="-1">
                 <div className="row">
-                    <div className="col text-left name-group">{item.title}</div>
-                    <div className="col text-right">
-                        <button type="button" className="btn" data-toggle="modal"
-                                data-target="#editDisciplineModal">
-                            <i className="fas fa-pencil-alt"></i>
-                        </button>
-                        <button type="button" className="btn" data-toggle="modal"
-                                data-target="#deleteDisciplineModal">
-                            <i className="fas fa-trash-alt"></i>
-                        </button>
-                    </div>
+                    <div className="col text-center name-group">Записей нет!</div>
                 </div>
             </li>
-            )
-        });
+        }
         return (
             <Fragment>
                 <div className="container-fluid ">
@@ -137,7 +65,16 @@ export class ListDiscipline extends Component {
                                 <p className="classic-title">Список дисциплин</p>
                             </div>
                             <ul className="list-group list-group-flush">
-                                {DisciplineArr}
+                                {
+                                    isLoading ?
+                                        <li className="list-group-item" key="-1">
+                                            <div className="row">
+                                                <div className="col text-center name-group">Загружаю...</div>
+                                            </div>
+                                        </li>
+                                        :
+                                        DisciplineList
+                                }
                             </ul>
                         </div>
                     </div>
