8957ada8923454a89bfc3218f99651673a852b37
AnatoliyCH <googlsocial97@gmail.com>
2019-03-14 22:09:03 +0700

    Student 22%

--- src/components/PageDisciplinePopup.js
+++ src/components/PageDisciplinePopup.js
@@ -1,157 +1,154 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from "react";
 import $ from "jquery";
-//БРАТЬ ТОКЕН ИЗ store
-export const PageDisciplinePopup = () => {
-    return (
-        <Fragment>
-            {/*Попап для добавления дисциплины*/}
-            <div className="modal fade" id="addDisciplineModal" tabIndex="-1" role="dialog"
-                 aria-labelledby="addDisciplineModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <form id="addDiscipline">
-                            <div className="modal-header bg-light">
-                                <h5 className="modal-title" id="addDisciplineModalLabel">Добавление дисциплины</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <div className="form-group">
-                                    <label htmlFor="addGroupCourse">Название дисциплины</label>
-                                    <input className="form-control" type="text" name="value" id="addDisciplineCourse"
-                                           placeholder="Введите название дисциплины"/>
+
+export class PageDisciplinePopup extends Component{
+    AddDiscipline = event =>{
+        event.preventDefault();
+        let newDiscipline = new FormData();
+        newDiscipline.append('name', document.getElementById("addDisciplineCourse").value);
+        fetch('http://nstu-tracker.thematrix.su/discipline', {
+            method: "POST",
+            headers: {
+                "api-token": localStorage.getItem('token'),
+            },
+            body: newDiscipline
+        }).then(function (response) {
+            return response.json()
+        }).then(data => {
+            $('#addDisciplineModal').modal('toggle');
+            setTimeout(() => {
+                document.location.reload(true);
+            }, 300);
+        }).catch(function (error) {
+            console.log('POST add discipline failed /n', error.message)
+        });
+    };
+    DelDiscipline = event =>{
+        event.preventDefault();
+        let DelItemId = localStorage.getItem('DelItemId');
+        let DelItemName = localStorage.getItem('DelItemName');
+        const delDiscipline = new FormData();
+        delDiscipline.append('name', DelItemName);
+        fetch('http://nstu-tracker.thematrix.su/discipline/' + DelItemId, {
+            method: "DELETE",
+            headers: {
+                "api-token": localStorage.getItem('token'),
+            },
+            body: delDiscipline
+        }).then(function (response) {
+            return response.json()
+        }).then(data => {
+            $('#deleteDisciplineModal').modal('toggle');
+            setTimeout(() => {
+                document.location.reload(true);
+            }, 300);
+        }).catch(function (error) {
+            console.log('DELETE discipline failed /n', error.message)
+        });
+        localStorage.setItem('DelItemId', '');
+        localStorage.setItem('DelItemName', '');
+    };
+    EditDiscipline = event =>{
+        event.preventDefault();
+        let EditItemId = localStorage.getItem('EditItemId');
+        fetch('http://nstu-tracker.thematrix.su/discipline/' + EditItemId, {
+            method: "PUT",
+            headers: {
+                "api-token": localStorage.getItem('token'),
+            },
+            body: new URLSearchParams({name: document.getElementById("editDisciplineNameLabel").value})
+        }).then(function (response) {
+            return response;
+        }).then(data => {
+            $('#editDisciplineModal').modal('toggle');
+            setTimeout(() => {
+                document.location.reload(true);
+            }, 300);
+        }).catch(function (error) {
+            console.log('EDIT discipline failed /n', error);
+        });
+        localStorage.setItem('EditItemId', '');
+    };
+    render(){
+        return (
+            <Fragment>
+                {/*Попап для добавления дисциплины*/}
+                <div className="modal fade" id="addDisciplineModal" tabIndex="-1" role="dialog"
+                     aria-labelledby="addDisciplineModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <form id="addDiscipline" onSubmit={this.AddDiscipline}>
+                                <div className="modal-header bg-light">
+                                    <h5 className="modal-title" id="addDisciplineModalLabel">Добавление дисциплины</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
                                 </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-outline-primary"
-                                        onClick={AddDiscipline}>Добавить
-                                </button>
-                                <button type="button" className="btn btn-outline-secondary"
-                                        data-dismiss="modal">Отмена
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            {/*Попап для удаления дисциплины*/}
-            <div className="modal fade" id="deleteDisciplineModal" tabIndex="-1" role="dialog"
-                 aria-labelledby="deleteDisciplineModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <form id="addDiscipline">
-                            <div className="modal-header bg-light">
-                                <h5 className="modal-title" id="deleteDisciplineModalLabel">Вы уверены, что хотите
-                                    удалить дисицплину?</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-outline-primary"
-                                        onClick={DelDiscipline}>Удалить
-                                </button>
-                                <button type="button" className="btn btn-outline-secondary"
-                                        data-dismiss="modal">Отмена
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            {/*Попап для редактирования дисциплины*/}
-            <div className="modal fade" id="editDisciplineModal" tabIndex="-1" role="dialog"
-                 aria-labelledby="editDisciplineModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <form id="editDiscipline">
-                            <div className="modal-header bg-light">
-                                <h5 className="modal-title" id="editDisciplineModalLabel">Редактирование дисциплины</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                                <div className="form-group">
-                                    <label htmlFor="editDisciplineName">Изменение дисциплины</label>
-                                    <input className="form-control" type="text" name="value"
-                                           id="editDisciplineNameLabel"
-                                           placeholder="Введите новое название дисциплины"/>
+                                <div className="modal-body">
+                                    <div className="form-group">
+                                        <label htmlFor="addGroupCourse">Название дисциплины</label>
+                                        <input className="form-control" type="text" name="value" id="addDisciplineCourse"
+                                               placeholder="Введите название дисциплины"/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-outline-primary"
-                                        onClick={EditDiscipline}>Применить
-                                </button>
-                                <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Отмена
-                                </button>
-                            </div>
-                        </form>
+                                <div className="modal-footer">
+                                    <button type="submit" className="btn btn-outline-primary">Добавить</button>
+                                    <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Отмена</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Fragment>
-    )
-};
-function AddDiscipline() {
-    const newDiscipline = new FormData();
-    newDiscipline.append('name', document.getElementById("addDisciplineCourse").value);
-    fetch('http://nstu-tracker.thematrix.su/discipline', {
-        method: "POST",
-        headers: {
-            "api-token": localStorage.getItem('token'),
-        },
-        body: newDiscipline
-    }).then(function (response) {
-        $('#addDisciplineModal').modal('toggle');
-        return response.json()
-    }).catch(function (error) {
-        console.log('POST add failed', error.message)
-    });
-    setTimeout(() => {
-        document.location.reload(true);
-    }, 500);
-}
-function DelDiscipline() {
-    let DelItemId = localStorage.getItem('DelItemId');
-    let DelItemName = localStorage.getItem('DelItemName');
-    const delDiscipline = new FormData();
-    delDiscipline.append('name', DelItemName);
-    fetch('http://nstu-tracker.thematrix.su/discipline/' + DelItemId, {
-        method: "DELETE",
-        headers: {
-            "api-token": localStorage.getItem('token'),
-        },
-        body: delDiscipline
-    }).then(function (response) {
-        $('#deleteDisciplineModal').modal('toggle');
-        return response.json()
-    }).catch(function (error) {
-        console.log('DELETE failed', error.message)
-    });
-    localStorage.setItem('DelItemId', '');
-    localStorage.setItem('DelItemName', '');
-    setTimeout(() => {
-        document.location.reload(true);
-    }, 500);
-}
-function EditDiscipline() {
-    let EditItemId = localStorage.getItem('EditItemId');
-    fetch('http://nstu-tracker.thematrix.su/discipline/' + EditItemId, {
-        method: "PUT",
-        headers: {
-            "api-token": localStorage.getItem('token'),
-        },
-        body: new URLSearchParams({name: document.getElementById("editDisciplineNameLabel").value})
-    }).then(function (response) {
-        $('#editDisciplineModal').modal('toggle');
-        return response;
-    }).catch(function (error) {
-        console.log('EDIT failed', error);
-    });
-    setTimeout(() => {
-        document.location.reload(true);
-    }, 500);
-    localStorage.setItem('EditItemId', '');
+                {/*Попап для удаления дисциплины*/}
+                <div className="modal fade" id="deleteDisciplineModal" tabIndex="-1" role="dialog"
+                     aria-labelledby="deleteDisciplineModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <form id="addDiscipline" onSubmit={this.DelDiscipline}>
+                                <div className="modal-header bg-light">
+                                    <h5 className="modal-title" id="deleteDisciplineModalLabel">Вы уверены, что хотите
+                                        удалить дисицплину?</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="submit" className="btn btn-outline-primary">Удалить</button>
+                                    <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Отмена</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                {/*Попап для редактирования дисциплины*/}
+                <div className="modal fade" id="editDisciplineModal" tabIndex="-1" role="dialog"
+                     aria-labelledby="editDisciplineModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <form id="editDiscipline" onSubmit={this.EditDiscipline}>
+                                <div className="modal-header bg-light">
+                                    <h5 className="modal-title" id="editDisciplineModalLabel">Редактирование дисциплины</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className="form-group">
+                                        <label htmlFor="editDisciplineName">Изменение дисциплины</label>
+                                        <input className="form-control" type="text" name="value"
+                                               id="editDisciplineNameLabel"
+                                               placeholder="Введите новое название дисциплины"/>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="submit" className="btn btn-outline-primary" >Применить</button>
+                                    <button type="button" className="btn btn-outline-secondary" data-dismiss="modal">Отмена</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
 }
